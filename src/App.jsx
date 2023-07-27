41a92a9baef1107832fc5e943f3831f3c4f5dd5a
Gennadiy Mostovskiy <pallyfan920@gmail.com>
2023-07-27 12:04:19 -0500

    add global styled component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,39 @@
+import styled from 'styled-components';
+import GlobalStyle from './styles/globalstyles';
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+  background-color: yellow;
+`;
+
+const Button = styled.button`
+  font-size: 1.5rem;
+  padding: 1.2rem 1.6rem;
+  font-weight: 500;
+  border: none;
+  border-radius: 7px;
+  background-color: tomato;
+  color: white;
+  margin: 20px;
+`;
+
+const StyledApp = styled.div`
+  background-color: orangered;
+  padding: 20px;
+`;
+
 function App() {
-  return <div>Heller</div>;
+  return (
+    <>
+      <GlobalStyle />
+      <StyledApp>
+        <H1>Heller</H1>
+        <Button onClick={() => alert('Check- IN')}>Check In</Button>
+        <Button onClick={() => alert('Check Out')}>Check Out</Button>
+      </StyledApp>
+    </>
+  );
 }
 
 export default App;
