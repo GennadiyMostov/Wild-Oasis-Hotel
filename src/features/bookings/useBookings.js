23e2f973e20678045b7d4ec7fd95a3fbbaf59f84
Gennadiy Mostovskiy <pallyfan920@gmail.com>
2023-08-04 10:52:05 -0500

    create prefetch functions for pagination

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,9 +1,11 @@
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
 import { getBookings } from '../../services/apiBookings';
 import { useSearchParams } from 'react-router-dom';
+import { PAGE_SIZE } from '../../utils/constants';
 
 const useBookings = () => {
   const [searchParams] = useSearchParams();
+  const queryClient = useQueryClient();
 
   //Filter
   const filterValue = searchParams.get('status');
@@ -30,6 +32,23 @@ const useBookings = () => {
     queryKey: ['bookings', filter, sortBy, page], //<-- This can be thought of as a dependency array
     queryFn: () => getBookings({ filter, sortBy, page }),
   });
+
+  //Pre-Fetching
+  const pageCount = Math.ceil(count / PAGE_SIZE);
+
+  if (page < pageCount) {
+    queryClient.prefetchQuery({
+      queryKey: ['bookings', filter, sortBy, page + 1],
+      queryFn: () => getBookings({ filter, sortBy, page: page + 1 }),
+    });
+  }
+
+  if (page > 1) {
+    queryClient.prefetchQuery({
+      queryKey: ['bookings', filter, sortBy, page - 1],
+      queryFn: () => getBookings({ filter, sortBy, page: page - 1 }),
+    });
+  }
 
   return { bookings, error, isLoading, count };
 };
