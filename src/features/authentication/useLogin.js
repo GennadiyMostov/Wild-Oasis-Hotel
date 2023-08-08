741cf7850fd4c2259881885d180328e83966d9a8
Gennadiy Mostovskiy <pallyfan920@gmail.com>
2023-08-08 13:14:02 -0500

    create login hook, auth api logic and implement to login form

---
+++ src/features/authentication/useLogin.js
@@ -0,0 +1,23 @@
+import { useMutation } from '@tanstack/react-query';
+import { login as loginApi } from '../../services/apiAuth';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-hot-toast';
+
+const useLogin = () => {
+  const navigate = useNavigate();
+  const { mutate: login, isLoading } = useMutation({
+    mutationFn: ({ email, password }) => loginApi({ email, password }),
+    onSuccess: (user) => {
+      console.log(user);
+      navigate('/dashboard');
+    },
+    onError: (error) => {
+      console.log('ERROR', error);
+      return toast.error('Email or Password Incorrect. Try Again.');
+    },
+  });
+
+  return { login, isLoading };
+};
+
+export default useLogin;
