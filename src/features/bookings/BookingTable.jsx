cee12862094e99a894255d5743cfd851795f0c08
Gennadiy Mostovskiy <pallyfan920@gmail.com>
2023-08-10 17:27:41 -0500

    fix intentional bug

--- src/features/bookings/BookingTable.jsx
+++ src/features/bookings/BookingTable.jsx
@@ -9,10 +9,11 @@ import Pagination from '../../ui/Pagination';
 function BookingTable() {
   const { bookings, isLoading, count } = useBookings();
 
+  if (isLoading) return <Spinner />;
+
   if (!bookings.length) {
     return <Empty resource='bookings' />;
   }
-  if (isLoading) return <Spinner />;
 
   return (
     <Menus>
